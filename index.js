92126010a7e6007f1265cb2e2d4fe4b8b9c49a58
jpa87 <justin.pan.westmount@gmail.com>
2018-07-23 19:36:37 -0700

    fixing heroku errors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var http = require('http');
 var app = express();
 var server = http.createServer(app).listen(3000);
-var io = require('socket.io');
+var io = require('socket.io')(server);
 
 app.use(express.static('./public'));
 

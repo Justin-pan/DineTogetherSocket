85bf1cbf163ca80d3031b5def5ec4c930e692015
jpa87 <justin.pan.westmount@gmail.com>
2018-07-25 15:14:36 -0700

    fixing heroku bugs and working on additional syntax problems

--- index.js
+++ index.js
@@ -4,21 +4,20 @@ var app = express();
 var server = http.createServer(app).listen(3000);
 var io = require('socket.io')(server);
 
-app.use(express.static('./public'));
-
 io.on('connection', function(socket){
   console.log("new connection");
-  socket.on('signIn', function(data){
-    socket.userName = data.name;
+  socket.on('signIn', function(name){
+    socket.userName = name;
   });
-  socket.on('joinRoom', function(data){
-    console.log("joined" + data.roomname);
+  socket.on('joinRoom', function(roomname){
+    console.log("joined" + roomname);
     var message = socket.userName + "has joined";
-    socket.join(data.roomname);
-    io.in(data.roomname).emit('joinmessage', message)
+    socket.join(roomname);
+    io.in(roomname).emit('joinmessage', message)
   });
-  socket.on('message', function(data){
-    console.log("message sent to" + data.roomname)
-    socket.broadcast.to(data.roomname).emit('message', { roomname: data.roomname, email: data.email, name: socket.userName, message: data.message });
+  socket.on('message', function(roomname, message, email, name){
+    console.log("message sent to" + roomname)
+    socket.broadcast.to(roomname).emit('message', roomname, message, email, name);
   });
 });
+console.log("server listening on 3000")
